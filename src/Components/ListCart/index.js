3dc91955bd703f1c8421525e31a018a370d7834e
オアイン <134583482+vuoanh291104@users.noreply.github.com>
2025-02-24 16:47:25 +0700

    ~fix cart page

--- src/Components/ListCart/index.js
+++ src/Components/ListCart/index.js
@@ -1,38 +1,68 @@
 import { Checkbox } from 'antd';
 import CartItem from '../CartItem';
 import './style.css'
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 function ListCart() {
 
     const[isChecked, setChecked] = useState(false)
+    const [isScrollDown, setScroll] = useState (false)
+    const listCartRef = useRef(null);
+
     const hanldeChecked = (e) => {
         setChecked(e.target.checked)
+        
     }
     
+    useEffect(()=> {
+        const hanldeScroll = () => {
+            if(!listCartRef.current) return;
+            const positionCart = listCartRef.current.getBoundingClientRect();
+            const windowHeight = window.innerHeight;
+            setScroll(positionCart.bottom <= windowHeight)
+        }
+        window.addEventListener('scroll',hanldeScroll)
+        return () => {
+            window.removeEventListener('scroll', hanldeScroll)
+        }
+    },[])
     return ( 
-        <div className="listCart">
-            <div className="listCartHead">
-                <p className='text1' >
-                    <Checkbox 
-                        style={{marginRight:'40px'}}
-                        checked = {isChecked}
-                        onChange = {hanldeChecked}
-                    >
-                    </Checkbox> 
-                    Sản phẩm </p>
-                <p >Đơn giá</p>
-                <p >Số lượng</p>
-                <p >Số tiền</p>
-                <p>Thao tác</p>
+        <div>
+            <div ref={listCartRef} className="listCart">
+                <div className="listCartHead">
+                    <p className='text1' >
+                        <Checkbox 
+                            style={{marginRight:'40px'}}
+                            checked = {isChecked}
+                            onChange = {hanldeChecked}
+                        >
+                        </Checkbox> 
+                        Sản phẩm </p>
+                    <p >Đơn giá</p>
+                    <p >Số lượng</p>
+                    <p >Số tiền</p>
+                    <p>Thao tác</p>
+                </div>
+                <div className="listCartBody">
+                    <CartItem 
+                        isChecked={isChecked}
+                        name=''
+                        cost = '10000'
+                        imageURL=''
+                    />
+                    <CartItem 
+                        isChecked={isChecked}
+                        name=''
+                        cost = '10000'
+                        imageURL=''
+                    />
+                    
+                </div>
             </div>
-            <div className="listCartBody">
-                <CartItem 
-                    isChecked={isChecked}
-                    name=''
-                    cost = '10000'
-                    imageURL=''
-                />
-                
+            <div className= {isScrollDown? 'toPayment':'toPaymentFix'}>
+                <Link to='/orderinfo'><button id='toPaymentBtn'>
+                    ĐẶT HÀNG
+                </button></Link>
             </div>
         </div>
     );
